c4fbb8dedd121f70eda906204fc367d1d7666c8d
cedric-holderick <cedric.holderick@gmail.com>
2016-08-17 21:29:29 +0200

    Update app.js

--- app.js
+++ app.js
@@ -24,3 +24,21 @@ $(window).scroll(function() {
    }
   });
 });
+
+
+var myCenter = new google.maps.LatLng(51.09169139999999,3.4370813000000453)
+function initialize() {
+var mapProp = {
+center:myCenter,
+zoom:12,
+scrollwheel:false,
+draggable:false,
+mapTypeId:google.maps.MapTypeId.ROADMAP
+};
+var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+var marker = new google.maps.Marker({
+position:myCenter,
+});
+marker.setMap(map);
+}
+google.maps.event.addDomListener(window, 'load', initialize);
