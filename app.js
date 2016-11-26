dc1ad4e1f8d92a5eadc5e8d15dcd288ac7ded61a
cedric-holderick <cedric.holderick@gmail.com>
2016-11-26 21:35:19 +0100

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
     var hash = this.hash;
     $('html, body').animate({
       scrollTop: $(hash).offset().top
-    }, 1250, function(){
+    }, 1550, function(){
 
       window.location.hash = hash;
       });
@@ -19,26 +19,8 @@ $(window).scroll(function() {
     $(".slideanim").each(function() {
     var pos = $(this).offset().top;
     var winTop = $(window).scrollTop();
-    if (pos < winTop + 600) {
+    if (pos < winTop + 500) {
          $(this).addClass("slide");
    }
   });
 });
-
-
-var myCenter = new google.maps.LatLng(51.09169139999999,3.4370813000000453)
-function initialize() {
-var mapProp = {
-center:myCenter,
-zoom:12,
-scrollwheel:false,
-draggable:false,
-mapTypeId:google.maps.MapTypeId.ROADMAP
-};
-var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-var marker = new google.maps.Marker({
-position:myCenter,
-});
-marker.setMap(map);
-}
-google.maps.event.addDomListener(window, 'load', initialize);
